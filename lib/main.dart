37c20e310f20ef5b00d0298ab63d99493eb3abcf
MukunthanPeriyasamy <mukunthanperiyasamy04@gmail.com>
2025-04-04 14:46:26 +0530

    changed theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      darkTheme: ThemeData(colorScheme: ColorScheme.light()),
+      darkTheme: ThemeData(colorScheme: ColorScheme.dark()),
       home: HomePage(),
   
       theme: ThemeData(fontFamily: 'Lato'),
